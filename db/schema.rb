363a20e3afe0d43ca43f908d826df5b8eeae0f20
Bryan <bryanporras2033@gmail.com>
2016-04-11 18:17:10 -0400

    create chefs and recipes tables

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408001132) do
+ActiveRecord::Schema.define(version: 20160411220852) do
+
+  create_table "chefs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "welcomes", force: :cascade do |t|
     t.datetime "created_at", null: false
